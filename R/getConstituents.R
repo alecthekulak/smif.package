4da86e39c245635eee2ba51dc080eabd54196893
alec25 <alecthekulak@gmail.com>
2017-10-03 21:17:02 -0400

    Updating files

--- R/getConstituents.R
+++ R/getConstituents.R
@@ -4,70 +4,119 @@
 #' Current \code{src} methods available are: default.
 #' Data is loaded silently without user assignment by default. 
 #' 
-#' @param index character specifying the name of the index
-#' @param env where to create objects. Setting env=NULL is equal to auto.assign=FALSE. Defaults to .GlobalEnv
+#' @param index a character vector specifying the name of the index. Acceptable values are 
+#'   \code{c("S&P 500", "NASDAQ 100", "DJIA")}
+#' @param env where to create objects. Setting \code{env=NULL} is equal to \code{auto.assign=FALSE}. 
+#'   Defaults to \code{.GlobalEnv}
 #' @param simple should results include non-ticker data. If \code{TRUE}, returns character vector of \code{index}'s 
-#'   constituents. If \code{FALSE} returns an. Defaults to \code{TRUE}
+#'   constituents. If \code{FALSE} returns a data.frame containing the following columns: stock ticker, last price, 
+#'   index weight. Defaults to \code{TRUE}
 #' @param src character string specifying the source to retrieve the data from. As of 0.1-1, 
 #' the only source available is "default" which changes depending on the selected \code{index}. Defaults to "default"
 #' @param auto.assign should results be loaded to \code{env}. If \code{FALSE} return results. Defaults to \code{TRUE}
 #' 
-#' @return if \code{auto.assign==FALSE}:  a character vector of \code{index}'s constituents
-#' @return if \code{auto.assign==TRUE}:  the name of the variable holding the data within the specified environment 
+#' @return a character vector or data.frame of \code{index}'s constituent tickers (, price, weight)
+#'  
+#' @author Alec Kulakowski, \email{alecthekulak@gmail.com}
+#' @references \url{http://us.spdrs.com/}, \url{http://www.nasdaq.com/quotes/nasdaq-100-stocks.aspx}
+#' @seealso \code{\link{smif.package}}
+#' @aliases getConstituents.simple
+#' 
+#' @details 
+#' \code{getConstituents} will try to coerce \code{index} to match one a reference string, corresponding to each index
+#' available to be searched. Data is loaded from various sources depending on input arguments: 
+#' 
+#' For S&P 500 data, the source is the \href{http://us.spdrs.com/}{SPDR website}, price data. For 
+#'   \code{SIMPLE=FALSE}, data is also drawn from Yahoo Finance.
+#' 
+#' For NASDAQ 100 data, the source is the \href{http://www.nasdaq.com/quotes/nasdaq-100-stocks.aspx}{NASDAQ website}. 
+#'   For \code{SIMPLE=FALSE}, price data is also taken from NASDAQ. Constituent weightings are not available in a
+#'   form scrapable by R. 
+#'   
+#' For DJIA 30 data, the souce is \href{http://money.cnn.com/data/dow30/}{CNN Money}, which updates its data every few
+#'   months. For \code{SIMPLE=FALSE}, up-to-date price data is sourced from Yahoo Finance, and constituent weightings 
+#'   are calculated manually. 
+#'   
+#' 
+#' @keywords misc
 #'  
 #' @export
 #' @examples 
-#' ## Not run: 
+#' \dontrun{
+#' 
 #' getConstituents(index='S&P500')
-#' ## End(Not run)
+#' getConstituents(index='NASDAQ 100')
+#' getConstituents(index='DJIA')
+#' }
 "getConstituents" <- function(index, env=.GlobalEnv, simple = TRUE, src = "default", auto.assign=TRUE){
   # suppressMessages(library(rvest))
   # suppressMessages(library(RCurl))
   # suppressMessages(library(gdata))
   # suppressMessages(library(quantmod))
   # suppressMessages(library(magrittr))
-  index <- tolower(index) %>% gsub(pattern='[ &^]+|index', replacement='')
+  # env=.GlobalEnv
+  # simple=TRUE
+  # src="default"
+  # auto.assign=TRUE
+  # index="S&P500"
+  index <- tolower(index) %>% gsub(pattern='[ &^]+|index', replacement='') #View(indexData)
   if(index %in% c('nasdaq100', 'ndx', 'nasdaq')){
-    indexData <- read.csv( url("http://www.nasdaq.com/quotes/nasdaq-100-stocks.aspx?render=download"),
+    # Retrieves NASDAQ-100 tickers from NASDAQ itself 
+    url <- "http://www.nasdaq.com/quotes/nasdaq-100-stocks.aspx?render=download"
+    indexData <- read.csv( url(url),
                            header=TRUE, strip.white=TRUE, stringsAsFactors = FALSE)
+    # Data cleaning 
+    indexData <- indexData[, !(names(indexData) %in% c("X"))]
     if(simple){
-      return(indexData$Symbol)
+      RESULT <- indexData$Symbol
     }else{
-      indexData$dollarVal <- indexData$share_volume * indexData $lastsale
-      totalVal <- sum(indexData$dollarVal)
-      indexData$Weight <- indexData$dollarVal / totalVal
+      # There is no current way to get weights for NASDAQ constituents (w/o advanced formulas)
+      # url2 <- "http://business.nasdaq.com/Docs/NDX.pdf"
+      indexData$Weight <- 0
       indexData <- indexData[,c('Symbol','lastsale','Weight')]
       colnames(indexData) <- c('Ticker','Price','Weight')
-      return(indexData)
+      warning("Weight data is not yet available for the NASDAQ 100")
+      RESULT <- indexData
     }
   }else if(index %in% c('sp500','gspc', 'spy', 'sp')){
+    # Retrieves S&P 500 tickers from SPDR site itself 
     url <- "http://us.spdrs.com/site-content/xls/SPY_All_Holdings.xls?fund=SPY&docname=All+Holdings&onyx_code1=&onyx_code2="
-    indexData <- gdata::read.xls("http://us.spdrs.com/site-content/xls/SPY_All_Holdings.xls?fund=SPY&docname=All+Holdings&onyx_code1=&onyx_code2=", 
+    indexData <- gdata::read.xls(url, 
                           header=FALSE,strip.white=TRUE, stringsAsFactors = FALSE)[-1:-4,]
     colnames(indexData) <- c('Company', 'Ticker', 'Weight', 'Sector', 'Shares')
-    indexData <- head(indexData, -10)
+    # indexData <- head(indexData, -10) #should be -11
     # url <- "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
     # web_page <- read_html(url) %>% html_node("table.wikitable") %>% html_table() 
     # sp_tickers <- unique(web_page[,'Ticker symbol'])
+    # Data cleaning (removing description lines in the XLS)
+    indexData <- indexData[indexData$Ticker != "",]
     if(simple){
-      
-      return(indexData$Ticker[indexData$Ticker != ""])
+      RESULT <- indexData$Ticker
     }else{
-      indexData$Price <- quantmod::getQuote(indexData$Ticker, what = yahooQF('Last Trade (Price Only)'))$Last
-      return(indexData[c('Ticker','Price', 'Weight')])
+      # Retrieve price data for each ticker 
+      indexData$Price <- quantmod::getQuote(indexData$Ticker, what = quantmod::yahooQF('Last Trade (Price Only)'))$Last
+      RESULT <- indexData[c('Ticker','Price', 'Weight')]
     }
   }else if(index %in% c('djia', 'dia', 'dji','dow','dowjones')){
+    # Retrieves Dow Jones Industrial Average tickers from CNN 
     url <- "http://money.cnn.com/data/dow30/"
-    dow_tickers <- read_html(url) %>% html_node('table.wsod_dataTable') %>% html_table()
-    dow_tickers$Ticker <- dow_tickers$Company %>% gsub(pattern='[[:blank:]]+[[:print:]]+', replacement='')
+    indexData <- xml2::read_html(url) %>% rvest::html_node('table.wsod_dataTable') %>% rvest::html_table()
+    indexData$Ticker <- indexData$Company %>% gsub(pattern='[[:blank:]]+[[:print:]]+', replacement='')
     if(simple){
-      return(dow_tickers$Ticker)
+      RESULT <- indexData$Ticker
     }else{
-      denom <- sum(dow_tickers$Price)
-      dow_tickers$Weight <- dow_tickers$Price/denom
-      return(dow_tickers[,c('Ticker','Price','Weight')])
+      # Tries to generate weights for each ticker
+      warning("Weights for DJIA constituents may not be accurate. Expect deviation <= 5%")
+      indexData$Price <- quantmod::getQuote(indexData$Ticker, what = quantmod::yahooQF('Last Trade (Price Only)'))$Last
+      denom <- sum(indexData$Price)
+      indexData$Weight <- indexData$Price/denom
+      RESULT <- indexData[,c('Ticker','Price','Weight')]
     }
+  }else{
+    stop("Invalid input")
   }
+  
+  return(RESULT)
   #} else if(index %in% c('russel3000', 'rusell3000', 'rusel3000', 'russell3000')){
   #   https://institutional.vanguard.com/#
   # }
---
+++ R/smif.package.R
@@ -0,0 +1,9 @@
+#' \code{smif.package} allows members of the SMIF Risk and Asset Allocation teams to store commonly used functions and
+#' datasets for use accross multiple devices and within RShiny projects. The package will be hosted on github, and can 
+#' be downloaded with the following code: 
+#' 
+#'   \code{devtools::install_github('alec25/smif.package')} 
+#' 
+#' The current functions held by \code{smif.package} are as follows: \code{\link{getConstituents}}
+#' 
+"_PACKAGE"
