8c7496201de3f40e5e51495980e9cc248d190724
alec25 <alecthekulak@gmail.com>
2017-10-04 12:27:23 -0400

    Bug fix update
    Fixed dependency on PERL installation, switch to using temporary files and the xlsx library

--- R/getConstituents.R
+++ R/getConstituents.R
@@ -1,54 +1,56 @@
-#' Load constituent data for indexes 
-#' 
-#' Functions to scrape and clean index constituent data from multiple sources for a given index. 
+#' Load constituent data for indexes
+#'
+#' Functions to scrape and clean index constituent data from multiple sources for a given index.
 #' Current \code{src} methods available are: default.
-#' Data is loaded silently without user assignment by default. 
-#' 
-#' @param index a character vector specifying the name of the index. Acceptable values are 
+#' Data is loaded silently without user assignment by default.
+#'
+#' @param index a character vector specifying the name of the index. Acceptable values are
 #'   \code{c("S&P 500", "NASDAQ 100", "DJIA")}
-#' @param env where to create objects. Setting \code{env=NULL} is equal to \code{auto.assign=FALSE}. 
+#' @param env where to create objects. Setting \code{env=NULL} is equal to \code{auto.assign=FALSE}.
 #'   Defaults to \code{.GlobalEnv}
-#' @param simple should results include non-ticker data. If \code{TRUE}, returns character vector of \code{index}'s 
-#'   constituents. If \code{FALSE} returns a data.frame containing the following columns: stock ticker, last price, 
+#' @param simple should results include non-ticker data. If \code{TRUE}, returns character vector of \code{index}'s
+#'   constituents. If \code{FALSE} returns a data.frame containing the following columns: stock ticker, last price,
 #'   index weight. Defaults to \code{TRUE}
-#' @param src character string specifying the source to retrieve the data from. As of 0.1-1, 
+#' @param src character string specifying the source to retrieve the data from. As of 0.1-1,
 #' the only source available is "default" which changes depending on the selected \code{index}. Defaults to "default"
 #' @param auto.assign should results be loaded to \code{env}. If \code{FALSE} return results. Defaults to \code{TRUE}
-#' 
-#' @return a character vector or data.frame of \code{index}'s constituent tickers (, price, weight)
-#'  
+#'
+#' @return a character vector or data.frame of \code{index}'s constituent tickers(, price, weight)
+#'
 #' @author Alec Kulakowski, \email{alecthekulak@gmail.com}
-#' @seealso \code{\link{smif.package}} \code{\link{cleanIndex}}
+#' @seealso \code{\link{smif.package}}, \code{\link{cleanIndex}}
 #' @aliases getConstituents
-#' 
-#' @details 
-#' \code{getConstituents} will try to format the \code{index} parameter using \code{\link{cleanIndex}}. 
-#' Data is loaded from various sources depending on input arguments: 
-#' 
-#' For S&P 500 data, the source is the \href{http://us.spdrs.com/}{SPDR website}, price data. For 
+#' @importFrom utils read.csv
+#' @import magrittr
+#'
+#' @details
+#' \code{getConstituents} will try to format the \code{index} parameter using \code{\link{cleanIndex}}.
+#' Data is loaded from various sources depending on input arguments:
+#'
+#' For S&P 500 data, the source is the \href{http://us.spdrs.com/}{SPDR website}, price data. For
 #'   \code{SIMPLE=FALSE}, data is also drawn from Yahoo Finance.
-#' 
-#' For NASDAQ 100 data, the source is the \href{http://www.nasdaq.com/quotes/nasdaq-100-stocks.aspx}{NASDAQ website}. 
+#'
+#' For NASDAQ 100 data, the source is the \href{http://www.nasdaq.com/quotes/nasdaq-100-stocks.aspx}{NASDAQ website}.
 #'   For \code{SIMPLE=FALSE}, price data is also taken from NASDAQ. Constituent weightings are not available in a
-#'   form scrapable by R. 
-#'   
+#'   form scrapable by R.
+#'
 #' For DJIA 30 data, the souce is \href{http://money.cnn.com/data/dow30/}{CNN Money}, which updates its data every few
-#'   months. For \code{SIMPLE=FALSE}, up-to-date price data is sourced from Yahoo Finance, and constituent weightings 
-#'   are calculated manually. 
-#'   
-#' Future versions will include \href{http://www.ftse.com/products/indices/russell-us}{Russel indexes} with data sourced 
+#'   months. For \code{SIMPLE=FALSE}, up-to-date price data is sourced from Yahoo Finance, and constituent weightings
+#'   are calculated manually.
+#'
+#' Future versions will include \href{http://www.ftse.com/products/indices/russell-us}{Russel indexes} with data sourced
 #'   from \href{https://institutional.vanguard.com/}{Vanguard}
 #'
-#' @keywords misc
-#'  
+#' @keywords misc data
+#'
 #' @export
-#' @examples 
+#' @examples
 #' \dontrun{
-#' 
+#'
 #' getConstituents(index='S&P 500')
 #' getConstituents(index='NASDAQ 100')
 #' getConstituents(index='DJIA', auto.assign=FALSE)
-#' 
+#'
 #' getConstituents.simple(index='S&P 500')
 #' }
 "getConstituents" <- function(index, env=.GlobalEnv, simple = TRUE, src = "default", auto.assign=TRUE){
@@ -62,11 +64,11 @@
   # Index text correction
   index = cleanIndex(index)
   if(index == "NDX"){
-    # Retrieves NASDAQ-100 tickers from NASDAQ itself 
+    # Retrieves NASDAQ-100 tickers from NASDAQ itself
     url <- "http://www.nasdaq.com/quotes/nasdaq-100-stocks.aspx?render=download"
     indexData <- read.csv( url(url),
                            header=TRUE, strip.white=TRUE, stringsAsFactors = FALSE)
-    # Data cleaning 
+    # Data cleaning
     indexData <- indexData[, !(names(indexData) %in% c("X"))]
     if(simple){
       RESULT <- indexData$Symbol
@@ -80,26 +82,26 @@
       RESULT <- indexData
     }
   }else if(index == "SPY"){
-    # Retrieves S&P 500 tickers from SPDR site itself 
+    # Retrieves S&P 500 tickers from SPDR site itself
     url <- "http://us.spdrs.com/site-content/xls/SPY_All_Holdings.xls?fund=SPY&docname=All+Holdings&onyx_code1=&onyx_code2="
-    indexData <- gdata::read.xls(url, 
+    indexData <- gdata::read.xls(url,
                           header=FALSE,strip.white=TRUE, stringsAsFactors = FALSE)[-1:-4,]
     colnames(indexData) <- c('Company', 'Ticker', 'Weight', 'Sector', 'Shares')
     # indexData <- head(indexData, -10) #should be -11
     # url <- "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
-    # web_page <- read_html(url) %>% html_node("table.wikitable") %>% html_table() 
+    # web_page <- read_html(url) %>% html_node("table.wikitable") %>% html_table()
     # sp_tickers <- unique(web_page[,'Ticker symbol'])
     # Data cleaning (removing description lines in the XLS)
     indexData <- indexData[indexData$Ticker != "",]
     if(simple){
       RESULT <- indexData$Ticker
     }else{
-      # Retrieve price data for each ticker 
+      # Retrieve price data for each ticker
       indexData$Price <- quantmod::getQuote(indexData$Ticker, what = quantmod::yahooQF('Last Trade (Price Only)'))$Last
       RESULT <- indexData[c('Ticker','Price', 'Weight')]
     }
   }else if(index == "DJIA"){
-    # Retrieves Dow Jones Industrial Average tickers from CNN 
+    # Retrieves Dow Jones Industrial Average tickers from CNN
     url <- "http://money.cnn.com/data/dow30/"
     indexData <- xml2::read_html(url) %>% rvest::html_node('table.wsod_dataTable') %>% rvest::html_table()
     indexData$Ticker <- indexData$Company %>% gsub(pattern='[[:blank:]]+[[:print:]]+', replacement='')
@@ -123,8 +125,9 @@
     return(RESULT)
   }
 }
-
-#' @describeIn getConstituents A one-input streamlined version of \code{getConstituents} 
-"getConstituents.simple" <- function(index){
+#' @rdname getConstituents
+#' A one-input streamlined version of \code{getConstituents} NOOO
+#' @return character vector
+"getConstituents.simple" <- function(index = "SPY"){
   getConstituents(index, simple=TRUE, src="default", auto.assign=FALSE)
 }
--- R/getSectorWeights.R
+++ R/getSectorWeights.R
@@ -1,54 +1,62 @@
 #' Load sector weightings for indexes
-#' 
-#' Function to scrape, clean, and format sector weightings for a given index or ETF. 
-#' 
+#'
+#' Function to scrape, clean, and format sector weightings for a given index or ETF.
+#'
 #' @param index a character vector specifying the name of the index. Values currently accepted are: \code{c("S&P 500")}
-#' 
-#' @return a data.frame containing \code{index}'s sector weightings 
-#' 
+#'
+#' @return a data.frame containing \code{index}'s sector weightings
+#'
 #' @author Alec Kulakowski, \email{alecthekulak@gmail.com}
 #' @seealso \code{\link{smif.package}}\code{\link{cleanIndex}}
-#' @aliases getSectorWeightings
-#' 
-#' @details 
-#' \code{getSectorWeights} will try to coerce the value of the \code{index} argument into a recognizable ticker using 
-#' \code{\link{cleanIndex}} before it attempts to access sector weightings. Data will be loaded from different sources 
+#' @aliases getSectorWeightings getSectorWeightings.SPY
+#' @import magrittr
+#' @importFrom utils download.file
+#'
+#' @details
+#' \code{getSectorWeights} will try to coerce the value of the \code{index} argument into a recognizable ticker using
+#' \code{\link{cleanIndex}} before it attempts to access sector weightings. Data will be loaded from different sources
 #' depending on input arguments:
-#' 
-#' For S&P 500 data, data will be retreived from the official 
+#'
+#' For S&P 500 data, data will be retreived from the official
 #' \href{"http://us.spindices.com/documents/additional-material/500-sector-representation.xlsx?force_download=true"}{S&P Dow Jones website}
-#' 
-#' @keywords misc
+#'
+#' @keywords misc data
 #' @export
-#' @examples 
+#' @examples
 #' \dontrun{
-#' 
+#'
 #' getSectorWeights("S&P 500")
 #' }
 "getSectorWeights" <- function(index = "SPY"){
   # Index text correction
   index = cleanIndex(index)
   if(index == "SPY"){
-    url <- "http://us.spindices.com/documents/additional-material/500-sector-representation.xlsx?force_download=true"
+    #http://us.spindices.com/documents/additional-material/500-sector-representation.xlsx?force_download=true
+    the_url <- "http://us.spindices.com/documents/additional-material/500-sector-representation.xlsx"
   }else{
     stop("Invalid input")
     return(NULL)
   }
   # Imports the raw data from the source
-  raw_data <- gdata::read.xls(url, header=TRUE, strip.white=TRUE, 
-                              blank.lines.skip=TRUE, pattern="MARKET REPRESENTATION", stringsAsFactors = FALSE)
+  # raw_data <- gdata::read.xls(the_url, header=TRUE, strip.white=TRUE,
+  #                             blank.lines.skip=TRUE, pattern="MARKET REPRESENTATION", stringsAsFactors = FALSE)
+  temp_file <- tempfile(fileext=".xlsx")
+  download.file(url = the_url, destfile = temp_file, method="wget")
+  raw_data<- xlsx::read.xlsx2(temp_file, sheetIndex = 1, startRow=4, header=TRUE, stringsAsFactors=FALSE)
+  #onwards
   new_data <- raw_data[1:11,1:2]
+
   # Fix formatting errors from souce within sector names
   sector_list <- unlist(lapply(new_data[,1],  function(SECTOR_STR){ paste(unlist(strsplit( SECTOR_STR, split="[*]?[ ]{1,}|[*]+")),collapse=" ") } ))
-  # Force percentages into decimal terms 
-  PercentComp <- unlist(lapply(new_data[,2], function(percent){as.numeric(sub("%", "", percent))/100}))
-  # Arrange final results in formatted dataframe 
+  # Force percentages into decimal terms
+  #  PercentComp <- unlist(lapply(new_data[,2], function(percent){as.numeric(sub("%", "", percent))}))
+  PercentComp <- unlist(lapply(new_data[,2], FUN=as.numeric()))
+  # Arrange final results in formatted dataframe
   RESULT <- as.data.frame(PercentComp, row.names = sector_list)
   return(RESULT)
 }
 "getSectorWeightings" <- getSectorWeights
 
-#' @describeIn getSectorWeights A no-input version of \code{getSectorWeights}. Runs \code{getSectorWeights} for the S&P 500. 
-#' This cannot be stored as a constant dataset as getSectorWeights is not a pure function when taken over long periods of time, as weights are variable.
+#' @describeIn getSectorWeights A no-input version of \code{getSectorWeights}. Runs \code{getSectorWeights} for the S&P 500. This cannot be stored as a constant dataset as getSectorWeights is not a pure function when taken over long periods of time, as weights are variable.
 "getSectorWeights.SPY" <- function(){ getSectorWeights("SPY") }
 "getSectorWeightings.SPY" <- getSectorWeights.SPY
